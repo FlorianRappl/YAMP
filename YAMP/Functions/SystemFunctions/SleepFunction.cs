94f6b616324eceefafc1a6839335187dc26533b5
Anders Gustafsson, Cureos AB <anders@cureos.com>
2012-11-15 13:54:21 +0100

    Made SleepFunction portable, and added additional ignoreCase argument in Enum.Parse

--- YAMP/Functions/SystemFunctions/SleepFunction.cs
+++ YAMP/Functions/SystemFunctions/SleepFunction.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading;
+﻿using System.Threading;
 
 namespace YAMP
 {
@@ -8,15 +6,18 @@ namespace YAMP
 	[Kind(PopularKinds.System)]
 	class SleepFunction : SystemFunction
 	{
+	    private readonly StringValue _start = new StringValue("start");
+        private readonly StringValue _stop = new StringValue("stop");
+
 		[Description("Sets the computation thread on idle for the proposed time in milliseconds (ms).")]
 		[Example("sleep(150)", "Sleeps for 150ms and outputs the real waiting time in ms.")]
 		public ScalarValue Function(ScalarValue a)
 		{
-			var sw = new Stopwatch();
-			sw.Start();
-			Thread.Sleep(a.IntValue);
-			sw.Stop();
-			return new ScalarValue(sw.ElapsedMilliseconds);
+			var sw = new TimerFunction();
+			sw.Function(_start);
+		    using (var blocking = new ManualResetEvent(false)) blocking.WaitOne(a.IntValue);
+			sw.Function(_stop);
+			return new ScalarValue(sw.Function());
 		}
 	}
 }
