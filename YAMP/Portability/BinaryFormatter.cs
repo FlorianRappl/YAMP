00a1bdfe12aafbf3b2f6b05def96e4a6077ef1b6
Anders Gustafsson, Cureos AB <anders@cureos.com>
2012-11-19 22:38:01 +0100

    Added dummy implementations for Serializable etc.

---
+++ YAMP/Portability/BinaryFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace System.Runtime.Serialization.Formatters.Binary
+{
+    internal class BinaryFormatter
+    {
+        internal void Serialize(Stream serializationStream, Object graph) { }
+
+        internal object Deserialize(Stream serializationStream)
+        {
+            return null;
+        }
+    }
+}
