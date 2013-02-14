eba8b6182b42d95c885aca6a5923e57555d928af
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-02-14 15:05:40 +0100

    Updated portable projects to 1.2.2 final Explicitly used ManualResetEvent handle in PropmptFunction to ensure portability

--- YAMP/Functions/SystemFunctions/PromptFunction.cs
+++ YAMP/Functions/SystemFunctions/PromptFunction.cs
@@ -11,7 +11,7 @@ namespace YAMP
         [Example("prompt()", "Shows the user a prompt with a default message. Returns the user input as String.")]
         public StringValue Function()
         {
-            var handle = new EventWaitHandle(false, EventResetMode.ManualReset);
+            var handle = new ManualResetEvent(false);
             var e = new UserInputEventArgs(handle, "Your input is required");
             Parser.RaiseInputPrompt(Context, e);
             handle.WaitOne();
@@ -22,7 +22,7 @@ namespace YAMP
         [Example("prompt(\"Please enter something\")", "Shows the user a prompt with the message to Please enter something. Returns the user input as String.")]
         public StringValue Function(StringValue message)
         {
-            var handle = new EventWaitHandle(false, EventResetMode.ManualReset);
+            var handle = new ManualResetEvent(false);
             var e = new UserInputEventArgs(handle, message.Value);
             Parser.RaiseInputPrompt(Context, e);
             handle.WaitOne();
