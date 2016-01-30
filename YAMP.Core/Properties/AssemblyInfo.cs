009008ca56ec663647ad0e30c3e5a2b06cbe97ed
Florian Rappl <rappl.florian@gmail.com>
2016-01-30 01:16:06 +0100

    Removed obsolete targets

--- YAMP.Core/Properties/AssemblyInfo.cs
+++ YAMP.Core/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("YAMP")]
-[assembly: AssemblyDescription("Yet Another Math Parser (YAMP) Core System with M# scripting language and all base types and functions.")]
+[assembly: AssemblyDescription("Yet Another Math Parser (YAMP) Core System with all base types and functions.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Florian Rappl et al.")]
 [assembly: AssemblyProduct("YAMP")]
