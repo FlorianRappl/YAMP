07d844b6e137117955744ed08e757ed2d04cb13a
Florian Rappl <rappl.florian@gmail.com>
2016-01-30 16:34:35 +0100

    Added some tests for the YAMP sensors library

---
+++ YAMP.Sensors.Tests/Base.cs
@@ -0,0 +1,17 @@
+﻿namespace YAMP.Sensors.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    public abstract class Base
+    {
+        protected void Test(String query, Double result, Double prec = 0.0)
+        {
+            var parser = new Parser();
+            parser.LoadPlugin(typeof(AccFunction).Assembly);
+            var value = parser.Evaluate(query);
+            var real = ((ScalarValue)value).Re;
+            Assert.AreEqual(result, real, prec);
+        }
+    }
+}
---
+++ YAMP.Sensors.Tests/Functions.cs
@@ -0,0 +1,54 @@
+﻿namespace YAMP.Sensors.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Functions : Base
+    {
+        [Test]
+        public void AccelerationWorksAndDoesNotChangeConsecutively()
+        {
+            Test("|acc() - acc()|", 0.0);
+        }
+        [Test]
+        public void GpsWorksAndDoesNotChangeConsecutively()
+        {
+            Test("|gps() - gps()|", 0.0);
+        }
+        [Test]
+        public void CompassWorksAndDoesNotChangeConsecutively()
+        {
+            Test("|comp() - comp()|", 0.0);
+        }
+        [Test]
+        public void LightnessWorksAndDoesNotChangeConsecutively()
+        {
+            Test("|light() - light()|", 0.0);
+        }
+        [Test]
+        public void InclineWorksAndDoesNotChangeConsecutively()
+        {
+            Test("|inc() - inc()|", 0.0);
+        }
+        [Test]
+        public void GyroWorksAndDoesNotChangeConsecutively()
+        {
+            Test("|gyro() - gyro()|", 0.0);
+        }
+        [Test]
+        public void OrientationWorksAndDoesNotChangeConsecutively()
+        {
+            Test("|orient() - orient()|", 0.0);
+        }
+        [Test]
+        public void OrientIsExpected3x3Matrix()
+        {
+            Test("A=size(orient());sum(A)", 6.0);
+        }
+        [Test]
+        public void GyroIsExpected3x1Matrix()
+        {
+            Test("A=size(gyro());sum(A)", 4.0);
+        }
+    }
+}
