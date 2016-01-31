204ce9bb8cfebab8b32ed8b3bc402520404e5691
Florian Rappl <rappl.florian@gmail.com>
2016-01-31 04:10:01 +0100

    Changed assembly info

--- YAMP.Core/Properties/AssemblyInfo.cs
+++ YAMP.Core/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("YAMP")]
 [assembly: AssemblyDescription("Yet Another Math Parser (YAMP) Core System with all base types and functions.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Florian Rappl et al.")]
+[assembly: AssemblyCompany("Florian Rappl")]
 [assembly: AssemblyProduct("YAMP")]
 [assembly: AssemblyCopyright("(c) Florian Rappl, 2012 - 2016.")]
 [assembly: AssemblyTrademark("")]
