5dc1a5308227aa170d79b1f2a4fe4aa39662fa05
Florian Rappl <rappl.florian@gmail.com>
2016-02-07 00:07:58 +0100

    Enable using objects (dictionaries)

---
+++ YAMP.Core/Functions/Helpers/ObjectFunction.cs
@@ -0,0 +1,12 @@
+﻿namespace YAMP.Functions.Helpers
+{
+    [Description("Creates an empty object.")]
+    [Kind(PopularKinds.Function)]
+    sealed class ObjectFunction : BaseFunction
+    {
+        public override Value Perform(Value argument)
+        {
+            return new ObjectValue();
+        }
+    }
+}
---
+++ YAMP.Core/Values/Arguments/ObjectValue.cs
@@ -0,0 +1,185 @@
+﻿namespace YAMP
+{
+    using System;
+    using System.Collections.Generic;
+    using YAMP.Exceptions;
+
+    /// <summary>
+    /// The class for representing an object value.
+    /// </summary>
+    public sealed class ObjectValue : Value, IFunction, ISetFunction
+    {
+        #region Fields
+
+        readonly Dictionary<String, Value> _values;
+
+        #endregion
+
+        #region ctor
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        public ObjectValue()
+            : this(new Dictionary<String, Value>())
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance and sets the values.
+        /// </summary>
+        /// <param name="values">The initial values to use.</param>
+        public ObjectValue(Object values)
+            : this(values.ToDictionary())
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance and sets the values.
+        /// </summary>
+        /// <param name="values">The initial values to use.</param>
+        public ObjectValue(Dictionary<String, Value> values)
+        {
+            _values = values;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the keys of the object.
+        /// </summary>
+        public IEnumerable<String> Keys
+        {
+            get { return _values.Keys; }
+        }
+
+        /// <summary>
+        /// Gets the number of entries.
+        /// </summary>
+        public Int32 Length
+        {
+            get { return _values.Count; }
+        }
+
+        /// <summary>
+        /// Gets the value for the given key.
+        /// </summary>
+        public Value this[String key]
+        {
+            get { return _values[key]; }
+        }
+
+        #endregion
+
+        #region Serialization
+
+        /// <summary>
+        /// Returns a copy of this object value instance.
+        /// </summary>
+        /// <returns>The cloned object value.</returns>
+        public override Value Copy()
+        {
+            var clone = new Dictionary<String, Value>(_values);
+            return new ObjectValue(clone);
+        }
+
+        /// <summary>
+        /// Converts the given value into binary data.
+        /// </summary>
+        /// <returns>The bytes array containing the data.</returns>
+        public override Byte[] Serialize()
+        {
+            using (var ms = Serializer.Create())
+            {
+                ms.Serialize(Length);
+
+                foreach (var element in _values)
+                {
+                    ms.Serialize(element.Key);
+                    ms.Serialize(element.Value);
+                }
+
+                return ms.Value;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new object value from the binary content.
+        /// </summary>
+        /// <param name="content">The data which contains the content.</param>
+        /// <returns>The new instance.</returns>
+        public override Value Deserialize(Byte[] content)
+        {
+            var dict = new Dictionary<String, Value>();
+
+            using (var ds = Deserializer.Create(content))
+            {
+                var length = ds.GetInt();
+
+                for (var i = 0; i < length; i++)
+                {
+                    var key = ds.GetString();
+                    var value = ds.GetValue();
+                    dict[key] = value;
+                }
+            }
+
+            return new ObjectValue(dict);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The method used by YAMP to get a value from an object.
+        /// </summary>
+        /// <param name="context">The context where this is happening.</param>
+        /// <param name="argument">The key of the value to retrieve.</param>
+        /// <returns>The value that has been requested.</returns>
+        public Value Perform(ParseContext context, Value argument)
+        {
+            var key = argument as StringValue;
+
+            if (key == null)
+            {
+                throw new YAMPArgumentWrongTypeException(argument.Header, "String", String.Empty);
+            }
+
+            var name = key.Value;
+            var value = default(Value);
+
+            if (_values.TryGetValue(name, out value))
+            {
+                return value;
+            }
+
+            return new ObjectValue();
+        }
+
+        /// <summary>
+        /// Method used by YAMP to set a value to an object.
+        /// </summary>
+        /// <param name="context">The context where this is happening.</param>
+        /// <param name="argument">The key of the value to retrieve.</param>
+        /// <param name="value">The value to set.</param>
+        /// <returns>The value that has been set.</returns>
+        public Value Perform(ParseContext context, Value argument, Value value)
+        {
+            var key = argument as StringValue;
+
+            if (key == null)
+            {
+                throw new YAMPArgumentWrongTypeException(argument.Header, "String", String.Empty);
+            }
+
+            var name = key.Value;
+            _values[name] = value;
+            return value;
+        }
+
+        #endregion
+    }
+}
