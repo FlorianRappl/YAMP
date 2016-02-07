19208e6b6718e513e90451229eebc7db83b750f9
Florian Rappl <rappl.florian@gmail.com>
2016-02-07 11:49:25 +0100

    Provide more detailled help

--- YAMP.Core/Functions/Helpers/ObjectFunction.cs
+++ YAMP.Core/Functions/Helpers/ObjectFunction.cs
@@ -2,9 +2,11 @@
 {
     [Description("Creates an empty object.")]
     [Kind(PopularKinds.Function)]
-    sealed class ObjectFunction : BaseFunction
+    sealed class ObjectFunction : ArgumentFunction
     {
-        public override Value Perform(Value argument)
+        [Description("Creates an empty object without any keys.")]
+        [Example("object()", "Returns an empty object that can represents a key-value store.")]
+        public ObjectValue Function()
         {
             return new ObjectValue();
         }
