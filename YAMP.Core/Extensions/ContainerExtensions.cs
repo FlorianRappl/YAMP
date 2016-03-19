b74f0ae6ab9d9e0fc45d0ddcf573ce2e36bdaed2
Florian Rappl <rappl.florian@gmail.com>
2016-03-19 23:28:24 +0100

    Added extensions for finding symbols

--- YAMP.Core/Extensions/ContainerExtensions.cs
+++ YAMP.Core/Extensions/ContainerExtensions.cs
@@ -1,6 +1,8 @@
 ﻿namespace YAMP
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     static class ContainerExtensions
     {
@@ -22,6 +24,41 @@
             }
 
             return false;
+        }
+
+        public static IEnumerable<SymbolExpression> GetGlobalSymbols(this ContainerExpression container)
+        {
+            var locals = container.GetLocalSymbols();
+            var symbols = container.GetSymbols();
+            return symbols.Where(symbol => !locals.Contains(symbol));
+        }
+
+        public static IEnumerable<SymbolExpression> GetLocalSymbols(this ContainerExpression container)
+        {
+            //TODO
+            return Enumerable.Empty<SymbolExpression>();
+        }
+
+        public static IEnumerable<SymbolExpression> GetSymbols(this ContainerExpression container)
+        {
+            var list = new List<SymbolExpression>();
+            var op = container.Operator as ArgsOperator;
+            var expressions = container.Expressions;
+
+            if (expressions != null)
+            {
+                foreach (var expression in expressions)
+                {
+                    expression.CollectSymbols(list);
+                }
+            }
+
+            if (op != null)
+            {
+                op.Content.CollectSymbols(list);
+            }
+
+            return list;
         }
     }
 }
---
+++ YAMP.Core/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace YAMP
+{
+    using System.Collections.Generic;
+
+    static class ExpressionExtensions
+    {
+        public static void CollectSymbols(this Expression expression, List<SymbolExpression> list)
+        {
+            if (expression is ContainerExpression)
+            {
+                list.AddRange(((ContainerExpression)expression).GetSymbols());
+            }
+            else if (expression is SymbolExpression)
+            {
+                list.Add((SymbolExpression)expression);
+            }
+        }
+    }
+}
